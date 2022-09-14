63be7cc25d8359cb49a9a0d422145fd95f560ea3
KRE-95 <kainat.elahipad@gmail.com>
2022-09-14 15:47:29 +0200

    unit-test

--- app/src/test/java/com/example/diceroll/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroll/ExampleUnitTest.kt
@@ -10,8 +10,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @Test // @Test annotation lets the compiler know that the following method is a test, which lets it run as such
+            /**
+             *  fun addition_isCorrect() {
+                assertEquals(4, 2 + 2) // he assertEquals() function asserts that an expected value
+            }         should equal an actual value obtained through business logic.
+             *
+             */
+    fun genrate_number() {
+        val dice = Dice(6)
+        val  rollResult = dice.roll()
+       assertTrue("The Value of rollResult was not between 1 and 6", rollResult in 1..6 )
+
+
     }
 }
